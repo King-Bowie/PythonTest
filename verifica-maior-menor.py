492950a8d24a5315fa07113bb75f15d41aec0664
Reinaldo A Simoes <54004680+ziggy-data@users.noreply.github.com>
2022-04-12 10:30:37 -0300

    Add files via upload

---
+++ verifica-maior-menor.py
@@ -0,0 +1,13 @@
+soma = quant = media = maior = menor = 0
+for numero in range(0,5):
+    num = int(input('Digite um numero: '))
+    soma += num
+    quant += 1
+    if quant ==1:
+        maior = menor = num
+    else:
+        if num > maior:
+            maior = num
+        if num < menor:
+            menor = num
+    print(f'Menor valor: {menor}   Maior valor: {maior}')
