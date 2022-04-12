e1b0a93a3540cca8152b08f6b7ec5e7767b504d6
Reinaldo A Simoes <54004680+ziggy-data@users.noreply.github.com>
2022-04-12 10:33:03 -0300

    Update verifica-maior-menor.py

--- verifica-maior-menor.py
+++ verifica-maior-menor.py
@@ -2,7 +2,7 @@ maior = menor = 0
 for numero in range(0,5):
     num = int(input('Digite um numero: '))    
     quant += 1
-    if quant ==1:
+    if quant == 1:
         maior = menor = num
     else:
         if num > maior:
