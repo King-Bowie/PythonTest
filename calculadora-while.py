47ae99ccfe74f6f7a62927ea4bf05dddc7d5d305
Reinaldo A Simoes <54004680+ziggy-data@users.noreply.github.com>
2022-04-17 23:50:30 -0300

    Update calculadora-while.py

--- calculadora-while.py
+++ calculadora-while.py
@@ -9,20 +9,14 @@ while(opcao != 0):
     print("4. Divisão ")
 
     opcao = int(input("Opção: "))
+    a = float(input("Primeiro valor: "))
+    b = float(input("Segundo valor: "))
 
     if(opcao==1):
-        a = float(input("Primeiro valor: "))
-        b = float(input("Segundo valor: "))
         print("Soma: ", a + b)
     elif(opcao==2):
-        a = float(input("Primeiro valor: "))
-        b = float(input("Segundo valor: "))
         print("Subtracao: ", a - b)
     elif(opcao==3):
-        a = float(input("Primeiro valor: "))
-        b = float(input("Segundo valor: "))
         print("Multiplicacao: ", a * b)
     elif(opcao==4):
-        a = float(input("Primeiro valor: "))
-        b = float(input("Segundo valor: "))
         print("Divisao: ", a / b)
